1ce7dc92830fb551863653a5c72687fd3a9e88c6
JamesTV1996 <42476767+JamesTV1996@users.noreply.github.com>
2021-04-26 08:33:01 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,97 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Pirate Lending About Us">
+	 <meta name="keywords" content="Web Design, IT Professional">
+  	<meta name="author" content="James Vincent">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" />
+    <link rel="stylesheet" href="./css/default.css">
+	<link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+	<title>  Pirate Lending | About Us</title>
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Pirate Lending </span></h1> 
+        </div>&nbsp;<img src="./img/logo.png" width="50px;">
+        <nav>
+          <ul>
+            <li><a href="index.php">Homepage</a></li>
+            <li><a href="members.php">For Members</a></li>
+			<li><a href="loans.php">Loan Estimator</a></li>
+			<li class="current"><a href="about.php">About</a></li>
+            
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <!--<section id="newsletter">
+      <div class="container">
+        <h1>Subscribe To Our Newsletter</h1>
+        <form>
+          <input type="email" placeholder="Enter Email...">
+          <button type="submit" class="button_1">Subscribe</button>
+        </form>
+      </div>
+    </section>
+
+    <section id="main">
+      <div class="container">
+        <article id="main-col">
+          <h1 class="page-title">Services</h1>
+          <ul id="services">
+            <li>
+              <h3>Website Design</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $1,000 - $3,000</p>
+            </li>
+            <li>
+              <h3>Website Maintenance</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $250 per month</p>
+            </li>
+            <li>
+              <h3>Website Hosting</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mi augue, viverra sit amet ultricies at, vulputate id lorem. Nulla facilisi.</p>
+						  <p>Pricing: $25 per month</p>
+            </li>
+          </ul>
+        </article>
+
+        <aside id="sidebar">
+          <div class="dark">
+            <h3>Get A Quote</h3>
+            <form class="quote">
+  						<div>
+  							<label>Name</label><br>
+  							<input type="text" placeholder="Name">
+  						</div>
+  						<div>
+  							<label>Email</label><br>
+  							<input type="email" placeholder="Emial Address">
+  						</div>
+  						<div>
+  							<label>Message</label><br>
+  							<textarea placeholder="Message"></textarea>
+  						</div>
+  						<button class="button_1" type="submit">Send</button>
+					</form>
+          </div>
+        </aside>
+      </div>
+    </section>
+	-->
+    <footer>
+      <p>CSCI 4710/6710 - Group 7 - Final Project | Copyright &copy; 2021</p>
+    </footer>
+  </body>
+</html>
+
